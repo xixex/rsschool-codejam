d9c5b9cff3dedab7c74746bfc8bcdab2d53f97fe
xixex <mikhailsontsav@gmail.com>
2018-10-28 15:56:21 +0300

    solution: solve make

---
+++ make/make.js
@@ -0,0 +1,38 @@
+function make(...args) {
+    let values = [];
+    let currentResult = decider(args) || 0;
+
+    function f(...args2) {
+        currentResult = decider(args2) ;
+        return f;
+    }
+
+    function isFunc(argument) {
+        return {}.toString.call(argument) === '[object Function]';
+    }
+
+    function decider(args) {
+        for (let i = 0; i < args.length; i++) {
+            if (isFunc(args[i])) {
+                let result = values.reduce((accumulator, a) => args[i](accumulator, a));
+                values.splice(0,values.length,result);
+                return result;
+            } else {
+                values.push(args[i]);
+            }
+        }
+        return 0;
+
+    }
+
+    f.inspect = () => {
+        return currentResult || values;
+    };
+
+    f.toString = () => {
+        return currentResult || values;
+    };
+
+    return f;
+}
+
